e424187525d3b129caedcc71a9c360303d57a915
Alina <TwinklingStar1488@gmail.com>
2018-07-11 19:34:37 +0300

    Point Test

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+  @Test
+  public void testDistancePositive () {
+    Point p1 = new Point(3, 6);
+    Point p2 = new Point(7, 5);
+    Assert.assertEquals(p1.distance(p2), 4.12);
+  }
+  
+  @Test
+  public void testDistanceNegative () {
+    Point p1 = new Point(-2, 6);
+    Point p2 = new Point(7, -5);
+    Assert.assertEquals(p1.distance(p2), 14.21);
+  }
+}
