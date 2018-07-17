874f332da9312c9f7bb8475dd2202994f8b11f21
Alina <TwinklingStar1488@gmail.com>
2018-07-17 18:56:50 +0300

    replaces inheritance with delegation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -1,17 +1,16 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class ContactCreationTest extends TestBase {
 
     @Test
     public void testContactCreation() {
-        goToContactPage();
-        fillContactForm(new ContactData(
+        app.goToContactPage();
+        app.fillContactForm(new ContactData(
                 "firstname1", "lastname1", "address1", "phone1", "email1"));
-        submitToContactPage();
-        returnToContactPage();
+        app.submitToContactPage();
+        app.returnToContactPage();
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -6,11 +6,11 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
-    goToGroupPage();
-    initGroupCreation();
-    fillGroupForm(new GroupData("name1", "header1", "footer1"));
-    submitToGroupPage();
-    returnToGroupPage();
+    app.goToGroupPage();
+    app.initGroupCreation();
+    app.fillGroupForm(new GroupData("name1", "header1", "footer1"));
+    app.submitToGroupPage();
+    app.returnToGroupPage();
   }
 
 }
