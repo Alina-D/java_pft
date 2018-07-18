797cb771eb90deec9c1d176b11b485bec87b7c3b
Alina <TwinklingStar1488@gmail.com>
2018-07-18 06:19:51 +0300

    create GroupModification

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase {
+
+  @Test
+  public  void  testGroupModification() {
+    app.getNavigatorHelper().goToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("name1", "header1", "footer1"));
+    app.getGroupHelper().submitToGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
