fe51a0c2e2c7cbfa571d67384871fdc31b8dd41e
Alina <TwinklingStar1488@gmail.com>
2018-07-22 12:08:48 +0300

    WebDriver

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatorHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatorHelper.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigatorHelper extends HelperBase{
 
-  public NavigatorHelper(FirefoxDriver wd) {
+  public NavigatorHelper(WebDriver wd) {
     super(wd);
   }
 
