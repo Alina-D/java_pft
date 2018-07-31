1eb87b8330e4fa953767826e2a4a6ea69fdd89da
Alina <TwinklingStar1488@gmail.com>
2018-07-31 19:01:10 +0300

    list

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
   public static void main (String[] args) {
     String[] lang = {"Python", "Ruby", "JS", "Java"};
 
-    for (String l : lang) {
+    List<String> languages = Arrays.asList("Python", "Ruby", "JS", "Java");
+
+    for (int i =0; i < languages.size(); i++) {
+      System.out.println("Я хочу выучить " + languages.get(i));
+    }
+    for (String l : languages) {
       System.out.println("Я хочу выучить " + l);
     }
   }
