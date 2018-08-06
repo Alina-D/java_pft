9d9ba2d66bbc61d583132926fc57d531880d28e7
Alina <TwinklingStar1488@gmail.com>
2018-08-06 20:20:48 +0300

    list group and contact

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTest.java
@@ -1,18 +1,21 @@
 package ru.stqa.pft.addressbook.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() {
     app.getNavigatorHelper().goToContactPage();
-    int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().createContact(new ContactData(
             "firstname1", "lastname1", "address1", "phone1",
             "email1", "name1"), true);
-    int after = app.getContactHelper().getContactCount();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
-
 }
