10bbb61c3be41ff1eed3d749be5c974f5e70f9d7
Alina <TwinklingStar1488@gmail.com>
2018-09-06 12:11:37 +0300

    fix testRegistration with Mail

--- mantis/src/test/java/ru/stqa/pft/mantis/model/MailMessage.java
+++ mantis/src/test/java/ru/stqa/pft/mantis/model/MailMessage.java
@@ -5,7 +5,7 @@ public class MailMessage {
   public String to;
   public String text;
 
-  public MailMessage(String s, String text) {
+  public MailMessage(String to, String text) {
     this.to = to;
     this.text = text;
   }
